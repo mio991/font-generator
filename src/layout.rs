ba182c69d4050090085440673011c164bc503936
mio991 <mio991@mio991.de>
2023-08-30 20:01:01 +0200

    improved layout

--- src/layout.rs
+++ src/layout.rs
@@ -1,12 +1,21 @@
 use std::{
     cell::{RefCell, RefMut},
-    sync::Arc,
+    io::Cursor,
+    rc::Rc,
 };
 
+pub trait Layouted {
+    fn requires_another_pass(&self) -> bool;
+    fn pass(&mut self);
+}
+
+pub trait Layoutable {
+    fn layout(&self, layouter: &mut Layouter) -> Box<dyn Layouted>;
+}
 #[derive(Debug)]
 pub struct Layouter {
     current_length: usize,
-    buffer: Vec<Arc<RefCell<[u8]>>>,
+    buffer: Vec<Rc<RefCell<Cursor<Box<[u8]>>>>>,
 }
 
 impl Layouter {
@@ -17,11 +26,12 @@ impl Layouter {
         }
     }
 
-    pub fn reserve<const N: usize>(&mut self) -> Reservation<N> {
+    pub fn reserve(&mut self, size: usize) -> Reservation {
         let offset = self.current_length;
-        let buffer = Arc::new(RefCell::new([0; N]));
+        self.current_length += size;
 
-        self.current_length += N;
+        let buffer = Rc::new(RefCell::new(Cursor::new(vec![0; size].into())));
+
         self.buffer.push(buffer.clone());
 
         Reservation { offset, buffer }
@@ -30,8 +40,8 @@ impl Layouter {
     pub fn get_result(&self) -> Vec<u8> {
         let mut result = Vec::with_capacity(self.current_length);
 
-        for arc in self.buffer.iter() {
-            result.extend(arc.borrow().to_vec());
+        for buffer in self.buffer.iter() {
+            result.extend(buffer.borrow().get_ref().iter());
         }
 
         assert_eq!(result.len(), self.current_length);
@@ -41,70 +51,77 @@ impl Layouter {
 }
 
 #[derive(Debug)]
-pub struct Reservation<const N: usize> {
+pub struct Reservation {
     offset: usize,
-    buffer: Arc<RefCell<[u8; N]>>,
+    buffer: Rc<RefCell<Cursor<Box<[u8]>>>>,
 }
 
-impl<const N: usize> Reservation<N> {
+impl Reservation {
     pub fn offset(&self) -> usize {
         self.offset
     }
 
-    pub fn buffer(&mut self) -> RefMut<[u8; N]> {
-        let cell = Arc::as_ref(&self.buffer);
-        RefCell::borrow_mut(cell)
+    pub fn writer(&mut self) -> RefMut<impl std::io::Write> {
+        self.buffer.borrow_mut()
     }
-}
-
-pub trait Layouted {
-    fn requires_another_pass(&self) -> bool;
-    fn pass(&mut self);
-}
-
-pub trait Layoutable {
-    fn layout(&self, layouter: &mut Layouter) -> Box<dyn Layouted>;
 }
 
 #[cfg(test)]
 mod test {
+    use std::io::Write;
+
     use super::*;
 
     #[test]
     fn can_create_a_reservation() {
         let mut layouter = Layouter::new();
 
-        layouter.reserve::<8>();
+        layouter.reserve(8);
     }
 
     #[test]
     fn reservations_have_the_correct_offset() {
         let mut layouter = Layouter::new();
 
-        layouter.reserve::<8>();
+        layouter.reserve(8);
 
-        let reservation = layouter.reserve::<2>();
+        let reservation = layouter.reserve(2);
 
         assert_eq!(8, reservation.offset());
     }
 
     #[test]
-    fn can_write_to_a_reservation() {
+    fn can_write_to_a_reservation() -> std::io::Result<()> {
         let mut layouter = Layouter::new();
 
-        let mut reservation = layouter.reserve::<8>();
+        let mut reservation = layouter.reserve(8);
 
-        *reservation.buffer() = *b"abcdefgh";
+        reservation.writer().write_all(b"abcdefgh")?;
+
+        Ok(())
     }
 
     #[test]
-    fn result_contains_what_was_written() {
+    fn result_contains_what_was_written() -> std::io::Result<()> {
         let mut layouter = Layouter::new();
 
-        *layouter.reserve::<4>().buffer() = *b"abcd";
+        layouter.reserve(4).writer().write_all(b"abcd")?;
 
-        *layouter.reserve::<4>().buffer() = *b"efgh";
+        layouter.reserve(4).writer().write_all(b"efgh")?;
 
-        assert_eq!(layouter.get_result(), b"abcdefgh".to_vec());
+        let result = layouter.get_result();
+
+        assert_eq!(result, b"abcdefgh".to_vec());
+
+        Ok(())
+    }
+
+    #[test]
+    fn can_not_write_more_than_reserved() {
+        let mut layouter = Layouter::new();
+
+        let result = layouter.reserve(4).writer().write_all(b"abcdefg");
+
+        assert!(result.is_err());
     }
 }
